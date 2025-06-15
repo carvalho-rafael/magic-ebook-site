03b7fde9f64da90989a44b4126e83c629f3aac94
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2025-06-15 14:35:41 -0300

    footer checkout page

--- src/components/checkout/Footer/index.tsx
+++ src/components/checkout/Footer/index.tsx
@@ -1,11 +1,26 @@
+import { FaEnvelope, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div className="flex flex-col w-full px-4 py-8 text-right [&>p]:mb-0 [&>p]:mt-0 bg-black text-gray-400">
-      <p>
-        <b>Fale conosco!</b>
+    <div className="flex flex-col w-full px-4 py-8 items-center [&>p]:mb-0 bg-black text-gray-400 text-lg text-center">
+      <a
+        href="https://www.instagram.com/magicebook.oficial/"
+        className="flex gap-2 items-center"
+      >
+        <FaInstagram /> magicebook.oficial
+      </a>
+      <a
+        href="mailto:contato@magicebook.com.br"
+        className="flex gap-2 items-center"
+      >
+        <FaEnvelope /> contato@magicebook.com.br
+      </a>
+      <p className="text-sm">
+        <b>
+          Magic Ebook | O jeito mais fácil de vender e comprar ebooks na
+          internet!
+        </b>
       </p>
-      <p>Quaisquer dúvidas ou sugestões nos envie um email</p>
-      <p>contato@magicebook.com.br</p>
     </div>
   );
 }
